f952fe383310ca09154093a582b4f5514a81c33c
clayamore <clayamore>
2024-02-29 16:08:28 +0100

    Removed individual steamid edits, changed it to global

--- src/main.rs
+++ src/main.rs
@@ -131,9 +131,24 @@ impl eframe::App for App {
                 ui.columns(2,| uis| {
                     if self.vm.active {
                         egui::Frame::none().show(&mut uis[1], |ui| {
+                            let steam_id_text_edit = egui::widgets::TextEdit::singleline(&mut self.vm.steam_id)
+                            .char_limit(17)
+                            .desired_width(125.);
                             ui.with_layout(egui::Layout::right_to_left(egui::Align::Center), |ui| {
                                 ui.label(format!("Character: {}", self.vm.profile_summary[self.vm.index].character_name));
-                                ui.label(format!("Steam Id: {}", self.vm.steam_id));
+                                
+                                match self.save.save_type {
+                                    SaveType::Unknown => {},
+                                    SaveType::PC(_) => {
+                                        let steam_id_text_edit = ui.add(steam_id_text_edit).labelled_by(ui.label("Steam Id:").id);
+                                        if steam_id_text_edit.hovered() {
+                                            egui::popup::show_tooltip(ui.ctx(), steam_id_text_edit.id, |ui|{
+                                                ui.label(egui::RichText::new("Important: This needs to match the id of the steam account that will use this save!").size(8.0).color(Color32::PLACEHOLDER));
+                                            });
+                                        }
+                                    },
+                                    SaveType::PlayStation(_) => {},
+                                };
                             });
                         });
                     }
--- src/ui/general.rs
+++ src/ui/general.rs
@@ -1,18 +1,11 @@
 pub mod general {
-    use eframe::{egui::{self, Ui}, epaint::Color32};
+    use eframe::egui::{self, Ui};
     use crate::vm::vm::vm::ViewModel;
 
 
     pub fn general(ui: &mut Ui, vm:&mut ViewModel) {
         ui.with_layout( egui::Layout::top_down_justified(egui::Align::Min),|ui|{
             
-            // Steam ID
-            ui.label("Steam Id:");
-            ui.text_edit_singleline(&mut vm.slots[vm.index].general_vm.steam_id);
-            ui.label(egui::RichText::new("Important: This needs to match the id of the steam account that will use this save!").size(8.0).color(Color32::PLACEHOLDER));
-            
-            ui.add_space(16.0);
-
             // Character Name
             ui.label("Character Name:");
             ui.add(egui::widgets::TextEdit::singleline(&mut vm.slots[vm.index].general_vm.character_name).char_limit(0x10));
