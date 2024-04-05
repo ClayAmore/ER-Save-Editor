7c67385472aed2ba4c8070fe3751532392f8d766
Colin Swenson-Healey <chozandrias76@github.com>
2024-04-04 23:27:57 -0700

    test: address PR feedback

--- src/vm/regions.rs
+++ src/vm/regions.rs
@@ -1,5 +1,5 @@
 pub mod regions_view_model {
-    use std::{collections::BTreeMap, path::PathBuf};
+    use std::{collections::BTreeMap, fs::File, io::Write, path::PathBuf};
 
     use rfd::FileDialog;
 
@@ -29,8 +29,8 @@ pub mod regions_view_model {
     impl RegionsViewModel {
         fn region_file_dialog() -> FileDialog {
             FileDialog::new()
-                .add_filter("TXT", &["txt", "Cheat Engine Exported Region File"])
-                .add_filter("*", &["*", "All files"])
+                .add_filter("regions", &["regions", "Save Editor Exported Region File"])
+                .add_filter("*", &["*", "Cheat Engine Exported Region File"])
                 .set_directory("./")
         }
 
@@ -60,12 +60,15 @@ pub mod regions_view_model {
             regions_vm
         }
 
-        pub fn to_string(&self) -> String {
+        // Provides a string in the same format that is provided by The Grand Archives: Elden Ring -
+        // Invasion Regions Export.
+        // This string provides activated regions in the format of numbers for region ids with new lines
+        // delimiting the entries
+        pub fn as_string(&self) -> String {
             let mut text = String::from("");
 
             for (region, (activated, _, _,_)) in self.regions.iter() {
                 if *activated {
-                    // Matches the format provided by a CE table for Elden Ring
                     let (region_id, _,_,_,_,_) = &REGIONS.lock().unwrap()[region];
                     text.push_str(&region_id.to_string());
                     text.push('\n');
@@ -74,22 +77,65 @@ pub mod regions_view_model {
 
             return text;
         }
+
+        pub fn write_regions(&self, path: PathBuf) {
+            let path_with_extension = if path.extension().unwrap_or_default() != "regions" {
+                path.with_extension("regions")
+            } else {
+                path
+            };
+            let mut f = File::create(path_with_extension).expect("");
+            let region_string =  &self.as_string();
+            
+            f.write_all(&region_string.as_bytes()).expect("Failed to write file");
+        }
+
+        pub fn set_active_regions(&mut self, active_region_ids: &Vec<u32>){
+
+            for i in 0..active_region_ids.len() {
+                let key = &active_region_ids[i as usize];
+                
+                if ID_TO_REGION.lock().unwrap().contains_key(key) {
+                    let region = ID_TO_REGION.lock().unwrap()[key];
+                    self.regions.get_mut(&region).expect("").0 = true;
+                }
+            }
+        }
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use super::regions_view_model::RegionsViewModel;
+
     #[test]
-    fn test_from_save_with_enabled_ids() {
-        let mut slot = crate::save::common::save_slot::SaveSlot::default();
-        slot.regions.unlocked_regions = vec![];
+    fn test_as_string() {
+        let region_vm = RegionsViewModel::default();
+        let region_as_string = region_vm.as_string();
+
+        assert_eq!(region_as_string, "", "Should not contain any enabled regions")
+    }
+
+    #[test]
+    fn test_write_regions() {
+        let regions_vm = &crate::vm::regions::regions_view_model::RegionsViewModel::default();
+        let temp_file = tempfile::NamedTempFile::new().expect("Failed to create temporary file");
+
+        regions_vm.write_regions(temp_file.path().to_path_buf());
+        let file_contents = std::fs::read_to_string(temp_file.path()).expect("Failed to read temporary file");
+        assert_eq!(file_contents, "");
+    }
+
+    #[test]
+    fn test_set_active_regions() {
 
         let key = 6101000;
-        let enabled_ids = vec![key];
-        let regions_vm = &mut crate::vm::regions::regions_view_model::RegionsViewModel::from_save(&slot, Some(&enabled_ids));
+        let regions_vm = &mut crate::vm::regions::regions_view_model::RegionsViewModel::default();
 
         let region = crate::db::regions::regions::ID_TO_REGION.lock().unwrap()[&key];
 
+        assert!(!regions_vm.regions.get_mut(&region).expect("").0, "Region 6101000 should be locked");
+        regions_vm.set_active_regions(&vec![key]);
         assert!(regions_vm.regions.get_mut(&region).expect("").0, "Region 6101000 should be unlocked");
         assert_eq!(regions_vm.regions.values().filter(|&&(is_active, _, _, _)| is_active).count(), 1, "Only one region should be unlocked");
     }
